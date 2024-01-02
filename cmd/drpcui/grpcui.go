c162b1d4c5090a2989182f50a5cd44bb1e499ce5
heysachin <hellosachindev@gmail.com>
2024-01-02 17:11:53 +0530

    cleanup

--- cmd/drpcui/grpcui.go
+++ cmd/drpcui/grpcui.go
@@ -46,11 +46,7 @@ import (
 	"github.com/fullstorydev/grpcui/standalone"
 )
 
-<<<<<<<< HEAD:cmd/drpcui/grpcui.go
 var version = "1.0.3"
-========
-var version = "1.0.2-sdev-nightly"
->>>>>>>> master:cmd/sdev-grpcui/grpcui.go
 
 var (
 	exit = os.Exit
